764ec83771d181c5cc02275401ebc94108b6fce7
digikid <dev@digikid.ru>
2020-06-01 22:12:42 +0400

    Fix output path

--- gulp/tasks/bootstrap.js
+++ gulp/tasks/bootstrap.js
@@ -4,14 +4,14 @@ module.exports = (gulp, plugins, config) => {
     return done => {
         if (fs.existsSync(config.paths.src.sass.bootstrap)) {
             return gulp.src(config.paths.src.sass.bootstrap)
-                .pipe(plugins.changed(`${config.paths.output.vendor}/css`))
+                .pipe(plugins.changed(`${config.paths.output.vendor.css}`))
                 .pipe(plugins.plumber())
                 .pipe(plugins.sass())
                 .pipe(plugins.if(!config.unicode, plugins.sassUnicode()))
                 .pipe(plugins.autoprefixer())
                 .pipe(plugins.concat(config.files.bootstrap))
                 .pipe(plugins.cleanCss())
-                .pipe(gulp.dest(`${config.paths.output.vendor}/css`));
+                .pipe(gulp.dest(`${config.paths.output.vendor.css}`));
         } else {
             done();
         };
