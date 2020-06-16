51457b2c81a1a5be8d04d004e0e2905844067299
digikid <dev@digikid.ru>
2020-06-16 14:29:54 +0400

    Minify polyfills

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,7 +1,7 @@
 module.exports = (gulp, plugins, config) => {
     return done => {
 
-        const jsCompiler = (path, merge, cb) =>
+        const jsCompiler = (path, merge, minify, cb) =>
             gulp.src(path)
                 .pipe(plugins.changed(config.paths.output.js))
                 .pipe(plugins.plumber())
@@ -15,7 +15,7 @@ module.exports = (gulp, plugins, config) => {
                 }))
                 .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.init()))
                 .pipe(plugins.if(merge, plugins.if(path === config.paths.src.js.polyfills, plugins.concat(config.files.polyfills), plugins.concat(config.files.js))))
-                .pipe(plugins.if(config.minify.js, plugins.uglify(), plugins.beautify.js({
+                .pipe(plugins.if(minify || config.minify.js, plugins.uglify(), plugins.beautify.js({
                     indent_size: 4,
                     max_preserve_newlines: 2
                 })))
@@ -26,10 +26,10 @@ module.exports = (gulp, plugins, config) => {
                 .pipe(gulp.dest(config.paths.output.js))
                 .on(`end`, cb);
 
-        const compileJsAll = cb => jsCompiler([config.paths.src.js.base, config.paths.src.js.components], true, cb);
-        const compileJsBase = cb => jsCompiler(config.paths.src.js.base, true, cb);
-        const compileJsComponents = cb => jsCompiler(config.paths.src.js.components, false, cb);
-        const compileJsPolyfills = cb => jsCompiler(config.paths.src.js.polyfills, true, cb);
+        const compileJsAll = cb => jsCompiler([config.paths.src.js.base, config.paths.src.js.components], true, false, cb);
+        const compileJsBase = cb => jsCompiler(config.paths.src.js.base, true, false, cb);
+        const compileJsComponents = cb => jsCompiler(config.paths.src.js.components, false, false, cb);
+        const compileJsPolyfills = cb => jsCompiler(config.paths.src.js.polyfills, true, true, cb);
 
         const tasks = config.merge ? [compileJsPolyfills, compileJsAll] : [compileJsPolyfills, compileJsBase, compileJsComponents];
 
