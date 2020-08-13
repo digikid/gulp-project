fe11d61df6c695b4e1016b32daa94081f0d710b4
digikid <dev@digikid.ru>
2020-08-13 23:28:36 +0400

    Add localization

---
+++ src/javascripts/language/fancybox.js
@@ -0,0 +1,14 @@
+$.fancybox.defaults.i18n.ru = {
+    CLOSE: 'Закрыть',
+    NEXT: 'Вперед',
+    PREV: 'Назад',
+    ERROR: 'Произошла ошибка при загрузке контента. Пожалуйста, попробуйте позже.',
+    PLAY_START: 'Слайдшоу',
+    PLAY_STOP: 'Стоп',
+    FULL_SCREEN: 'На весь экран',
+    THUMBS: 'Превью',
+    DOWNLOAD: 'Скачать',
+    SHARE: 'Поделиться',
+    ZOOM: 'Приблизить'
+};
+$.fancybox.defaults.lang = 'ru';
