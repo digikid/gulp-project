10cfde70c783553682fd0a32e715c7a9dabd7ebc
digikid <dev@digikid.ru>
2020-11-18 18:42:26 +0400

    Code refactoring Unreachable code elimination

--- src/javascripts/components/demo.js
+++ src/javascripts/components/demo.js
@@ -1,34 +1,16 @@
 $(document).ready(function() {
 
-    // run only in demo mode
+    // skip in build mode
     if (window._MODE !== 'build') {
 
         // disable form submit
         $('form').on('submit', function(e) {
             e.preventDefault();
-
-            if (!$(this).hasClass('form--booking')) {
-                $.fancybox.close();
-                $.fancybox.open($('#modal-success'));
-            };
+            $.fancybox.close();
+            $.fancybox.open($('#modal-success'));
         });
 
-        // copy button class
-        $('.page--ui .btn').each(function() {
-            if ($(this).closest('.section--buttons').length) {
-                try {
-                    new ClipboardJS($(this)[0], {
-                        text: function(trigger) {
-                            return $(trigger).attr('class');
-                        }
-                    });
-                } catch(e) {};
-            };
-        }).click(function(e) {
-            e.preventDefault();
-        });
-
-        // active menu
+        // set active menu item
         $('.menu__item').each(function() {
             var path = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
 
