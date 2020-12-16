04a2890172aa9910627f50ef735a59e3fd583218
digikid <dev@digikid.ru>
2020-12-16 21:13:51 +0400

    Code refactoring

--- src/javascripts/import/modules.js
+++ src/javascripts/import/modules.js
@@ -1,15 +1,13 @@
-require('intersection-observer');
-
 window.jQuery = window.$ = require('jquery');
 
 require('js.device.detector/dist/jquery.device.detector.js');
+require('intersection-observer');
+require('@fancyapps/fancybox');
+require('jquery-mask-plugin');
+require('@popperjs/core/dist/umd/popper.min.js');
+require('air-datepicker');
 
-const $ = require('jquery');
-const fancybox = require('@fancyapps/fancybox');
+const Swiper = require('swiper');
 const SimpleBar = require('simplebar');
 const lozad = require('lozad');
-const mask = require('jquery-mask-plugin');
-const Swiper = require('swiper');
-const popper = require('@popperjs/core/dist/umd/popper.min.js');
 const tippy = require('tippy.js/dist/tippy.umd.min.js');
-const datepicker = require('air-datepicker');
