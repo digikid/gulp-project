318b91b3a1f73758439879264f3b89964021adfb
digikid <dev@digikid.ru>
2021-03-12 15:16:35 +0400

    Code refactoring

--- src/javascripts/base/global/window.js
+++ src/javascripts/base/global/window.js
@@ -2,7 +2,7 @@
 window._DETECT = $.fn.deviceDetector.getInfo();
 
 // mode
-window._MODE = (location.hostname === 'localhost' || location.hostname === '127.0.0.1' || location.port === '9002') ? 'local' : location.host === 'demo.relevant.ru' ? 'demo' : 'build';
+window._MODE = location.hostname === 'localhost' || location.hostname === '127.0.0.1' || location.port === '9002' ? 'local' : location.host === 'demo.relevant.ru' ? 'demo' : 'build';
 
 // path to files
 window._TEMPLATES_PATH = document.location.origin + '/templates/';
